c859fbcef82c37425f95ca49995b5350f126e68e
Michael W. Kearney <kearneymw@missouri.edu>
2018-03-04 13:43:42 -0600

    added list method

--- R/textfeatures.R
+++ R/textfeatures.R
@@ -4,9 +4,11 @@ utils::globalVariables(c("text2", "n_chars", "text"))
 #'
 #' Extracts features from text vector.
 #'
-#' @param x Input data. Should be character vector or data frame/grouped data
-#'   frame with variable of interest named "text"
-#' @return A tibble data frame with extracted features as columns
+#' @param x Input data. Should be character vector, data frame, or grouped data
+#'   frame (grouped_df) with character variable of interest named "text". If
+#'   grouped_df is provided, then features will be averaged across all
+#'   observations for each group.
+#' @return A tibble data frame with extracted features as columns.
 #' @export
 textfeatures <- function(x) UseMethod("textfeatures")
 
@@ -17,24 +19,25 @@ textfeatures.character <- function(x) {
 }
 
 #' @export
+textfeatures.factor <- function(x) {
+  x <- as.character(x)
+  textfeatures(x)
+}
+
+#' @export
 #' @importFrom dplyr transmute
 textfeatures.data.frame <- function(x) {
+  ## validate input
   stopifnot("text" %in% names(x))
-  x$text2 <- gsub("https?:[[:graph:]]+|@\\S+|#\\S+", "", x$text)
-  ## exclamation mark
-  x$text2 <- gsub("\u00A1|\u01C3|\u202C|\u203D|\u2762", "\u0021", x$text2)
-  ## apostrophe
-  x$text2 <- gsub(
-    "\u2018|\u2019|\u05F3|\u02B9|\u02Bc|\u02C8|\u0301|\u05F3|\u2032|\uA78C",
-    "\u0027", x$text2)
-  ## comma
-  x$text2 <- gsub("\u2795", "\u002B", x$text2)
-  ## hyphen
-  x$text2 <- gsub("\u2010|\u2011|\u2012|\u2013|\u2043|\u2212|\u10191", "\u002D", x$text2)
-  ## period
-  x$text2 <- gsub("\u06D4|\u2E3C|\u3002", "\u002E", x$text2)
-  ## eplipses
-  x$text2 <- gsub("\u2026", "\u002E\u002E\u002E", x$text2)
+  ## make sure "text" is character
+  if (is.factor(x$text)) {
+    x$text <- as.character(x$text)
+  }
+  ## validate text class
+  stopifnot(is.character(x$text))
+  ## remove URLs, mentions, etc, and convert some non-ascii into ascii equiv
+  x <- text_cleaner(x)
+  ## extract features for all observations
   suppressMessages(dplyr::transmute(x,
     n_chars = n_charS(text2),
     n_commas = n_commas(text2),
@@ -60,21 +63,9 @@ textfeatures.data.frame <- function(x) {
 #' @importFrom dplyr transmute summarise_all
 textfeatures.grouped_df <- function(x) {
   stopifnot("text" %in% names(x))
-  x$text2 <- gsub("https?:[[:graph:]]+|@\\S+|#\\S+", "", x$text)
-  ## exclamation mark
-  x$text2 <- gsub("\u00A1|\u01C3|\u202C|\u203D|\u2762", "\u0021", x$text2)
-  ## apostrophe
-  x$text2 <- gsub(
-    "\u2018|\u2019|\u05F3|\u02B9|\u02Bc|\u02C8|\u0301|\u05F3|\u2032|\uA78C",
-    "\u0027", x$text2)
-  ## comma
-  x$text2 <- gsub("\u2795", "\u002B", x$text2)
-  ## hyphen
-  x$text2 <- gsub("\u2010|\u2011|\u2012|\u2013|\u2043|\u2212|\u10191", "\u002D", x$text2)
-  ## period
-  x$text2 <- gsub("\u06D4|\u2E3C|\u3002", "\u002E", x$text2)
-  ## eplipses
-  x$text2 <- gsub("\u2026", "\u002E\u002E\u002E", x$text2)
+  ## remove URLs, mentions, etc, and convert some non-ascii into ascii equiv
+  x <- text_cleaner(x)
+  ## extract features for all observations
   x <- suppressMessages(dplyr::transmute(x,
     n_chars = n_charS(text2),
     n_commas = n_commas(text2),
@@ -94,7 +85,52 @@ textfeatures.grouped_df <- function(x) {
     n_capsp = (n_caps + 1L) / (n_chars + 1L),
     n_charsperword = (n_chars + 1L) / (n_words + 1L)
   ))
+  ## summarise (via mean) by group and return features data
   dplyr::summarise_all(x, mean, na.rm = TRUE)
 }
 
+
+text_cleaner <- function(x) {
+  ## remove URLs, mentions, and hashtags
+  x$text2 <- gsub("https?:[[:graph:]]+|@\\S+|#\\S+", "", x$text)
+  ## convert non-ascii into ascii exclamation marks
+  x$text2 <- gsub("\u00A1|\u01C3|\u202C|\u203D|\u2762", "\u0021", x$text2)
+  ## convert non-ascii into ascii apostrophes
+  x$text2 <- gsub(
+    "\u2018|\u2019|\u05F3|\u02B9|\u02Bc|\u02C8|\u0301|\u05F3|\u2032|\uA78C",
+    "\u0027", x$text2)
+  ## convert non-ascii into ascii commas
+  x$text2 <- gsub("\u2795", "\u002B", x$text2)
+  ## convert non-ascii into ascii hypthens
+  x$text2 <- gsub("\u2010|\u2011|\u2012|\u2013|\u2043|\u2212|\u10191", "\u002D", x$text2)
+  ## convert non-ascii into ascii periods
+  x$text2 <- gsub("\u06D4|\u2E3C|\u3002", "\u002E", x$text2)
+  ## convert non-ascii into ascii elipses
+  x$text2 <- gsub("\u2026", "\u002E\u002E\u002E", x$text2)
+  ## return data frame with new text2 var
+  x
+}
+
+
+#' @export
+textfeatures.list <- function(x) {
+  ## if named list with "text" element
+  if (!is.null(names(x)) && "text" %in% names(x)) {
+    x <- x$text
+    return(textfeatures(x))
+    ## if all elements are character vectors, return list of DFs
+  } else if (all(lengths(x) == 1L) && all(vply_lgl(x, is.character))) {
+    ## (list in, list out)
+    return(lapply(x, textfeatures))
+  }
+  ## if all elements are recursive objects containing "text" variable
+  if (all(vply_lgl(x, is.recursive)) &&
+      all(vply_lgl(x, ~ "text" %in% names(.)))) {
+    x <- lapply(x, "[[", "text")
+    return(lapply(x, textfeatures))
+  }
+  stop(paste0("Input is a list without a character vector named \"text\". ",
+    "Are you sure the input shouldn't be a character vector or a data frame",
+    "with a \"text\" variable?"), call. = FALSE)
+}
 
--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -7,16 +7,28 @@ test_that("main textfeatures function", {
     "doh", "THe following list:\n- one\n- two\n- three\nOkay!?!",
     "asdf87. 68as7df6a as8\n  7df6asfd     87asdfasd &12 3091-.,'[] $ @asdf!!!"
   )
+  ## factor
+  text_fct <- factor(text)
   ## data frame with text variable
   df <- data.frame(text = text, stringsAsFactors = FALSE)
+  ## list of dfs with text variables
+  lst <- list(df, df, df)
 
   ## get text features of character vector
   o_chr <- textfeatures(text)
   expect_true(is.data.frame(o_chr))
 
-  ## get text features of character vector
+  ## get text features of factor
+  o_fct <- textfeatures(text_fct)
+  expect_true(is.data.frame(o_fct))
+
+    ## get text features of character vector
   o_df <- textfeatures(df)
   expect_true(is.data.frame(o_df))
+
+  ## get text features of list of DFs with "text" vars
+  o_lst <- textfeatures(lst)
+  expect_true(all(vapply(o_lst, is.data.frame, FUN.VALUE = logical(1))))
 
   ## shouldn't work if no variable named text
   expect_error(textfeatures(mtcars))
